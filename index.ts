30c43827609eff8ff70ed93deb9731983bd5b491
Mikolaj Dutkiewicz <mikolajdutkiewicz@gmail.com>
2023-02-06 18:59:14 +0100

    Added error handling and data base settings

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
 import express, { json } from "express";
 import cors from "cors";
 import "express-async-errors";
+import { handleError, ValidationError } from "./utils/errors";
 
 const app = express();
 
@@ -10,6 +11,14 @@ app.use(
   })
 );
 app.use(json());
+
+// routers...
+
+// app.get("/", async (req, res) => {
+//   throw new ValidationError("Daaamn!");
+// });
+
+app.use(handleError);
 
 app.listen(3001, "0.0.0.0", () => {
   console.log("Listening on http://localhost:3001");
