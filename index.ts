5c2d0b24d7372fc395305444e534489a8f071982
Mikolaj Dutkiewicz <mikolajdutkiewicz@gmail.com>
2023-02-18 15:21:12 +0100

    added prefix to all urls

--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
-import express, { json } from "express";
+import express, { json, Router } from "express";
 import cors from "cors";
 import rateLimit from "express-rate-limit";
 import "express-async-errors";
@@ -21,7 +21,11 @@ app.use(
   })
 );
 
-app.use("/ad", adRouter);
+const router = Router();
+
+router.use("/ad", adRouter);
+
+app.use("/api", router);
 
 app.use(handleError);
 
